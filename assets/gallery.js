622cff644f0bc3a610ccfa6040b6a03704197aad
Misir Jafarov <misir.ceferov@gmail.com>
2023-08-03 15:47:11 +0400

    lightGallery license key support

--- assets/gallery.js
+++ assets/gallery.js
@@ -4,7 +4,11 @@ $(gallery).justifiedGallery({
   margins: 5,
   border: -5
 }).on('jg.complete', function() {
-    lightGallery(gallery, {
-      plugins: [lgZoom],
-    });
+  const licenseKeyAttr = 'data-lightgallery-license-key';
+  const licenseKey = document.querySelector(`[${licenseKeyAttr}]`).getAttribute(licenseKeyAttr);
+
+  lightGallery(gallery, {
+    plugins: [lgZoom],
+    licenseKey: licenseKey
+  });
 });
